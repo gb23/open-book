4d1e61c39706b5a65b50d6792eafb3ed16b978f9
gb23 <gregbenj@hotmail.com>
2018-02-01 11:31:25 -0700

    create seed data for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Section.create(text: "One text ", votes: 1, next_ids: [1,3])
+
+Section.create(text: "Two text ", votes: 3, next_ids: [2])
+
+Section.create(text: "Three text ", votes: 1, next_ids: [4,5])
+
+Section.create(text: "Four text ", votes: 3, next_ids: [-1])
+
+Section.create(text: "Five text ", votes: 1, next_ids: [-1])
+
+Section.create(text: "Six text ", votes: 2, next_ids: [-1])
