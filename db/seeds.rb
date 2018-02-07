5051e11a2c016b6c464fe8083b5e2498b8b005ac
gb23 <gregbenj@hotmail.com>
2018-02-07 14:57:20 -0700

    add neg 1 values at beginning and end of next_ids array

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-Section.create(text: "One text ", votes: 1, next_ids: [2,4], prev_id: -1)
+Section.create(text: "One text ", votes: 1, next_ids: [-1,2,4,-1], prev_id: -1)
 
-Section.create(text: "Two text ", votes: 3, next_ids: [3], prev_id: 1)
+Section.create(text: "Two text ", votes: 3, next_ids: [-1,3,-1], prev_id: 1)
 
-Section.create(text: "Three text ", votes: 1, next_ids: [5,6], prev_id: 2)
+Section.create(text: "Three text ", votes: 1, next_ids: [-1,5,6,-1], prev_id: 2)
 
-Section.create(text: "Four text ", votes: 3, next_ids: [-1], prev_id: 1)
+Section.create(text: "Four text ", votes: 3, next_ids: [-1,-1], prev_id: 1)
 
-Section.create(text: "Five text ", votes: 1, next_ids: [-1], prev_id: 3)
+Section.create(text: "Five text ", votes: 1, next_ids: [-1,-1], prev_id: 3)
 
-Section.create(text: "Six text ", votes: 2, next_ids: [-1], prev_id: 3)
+Section.create(text: "Six text ", votes: 2, next_ids: [-1,-1], prev_id: 3)
