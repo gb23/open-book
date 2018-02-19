95a0c19e8fba78c5563698fef103cc317591374f
gb23 <gregbenj@hotmail.com>
2018-02-19 10:55:26 -0700

    add second composition

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,15 @@
-Section.create(text: "One text ", votes: 1, next_ids: [2,4], prev_id: -1)
+comp_one = Composition.create(text: "composition1 text", votes: 1, next_ids: [1,7], prev_id: -1)
 
-Section.create(text: "Two text ", votes: 3, next_ids: [3], prev_id: 1)
+sec_one = Section.create(text: "One text ", composition_id: comp_one.id, votes: 1, next_ids: [2,4], prev_id: comp_one.id)
 
-Section.create(text: "Three text ", votes: 1, next_ids: [5,6], prev_id: 2)
+sec_two = Section.create(text: "Two text ", composition_id: comp_one.id, votes: 3, next_ids: [3], prev_id: sec_one.id)
 
-Section.create(text: "Four text ", votes: 3, next_ids: [-1], prev_id: 1)
+sec_three = Section.create(text: "Three text ", composition_id: comp_one.id, votes: 1, next_ids: [5,6], prev_id: 2)
 
-Section.create(text: "Five text ", votes: 1, next_ids: [-1], prev_id: 3)
+sec_four = Section.create(text: "Four text ", composition_id: comp_one.id, votes: 3, next_ids: [-1], prev_id: sec_one.id)
 
-Section.create(text: "Six text ", votes: 2, next_ids: [-1], prev_id: 3)
+sec_five = Section.create(text: "Five text ", composition_id: comp_one.id, votes: 1, next_ids: [-1], prev_id: 3)
+
+sec_six = Section.create(text: "Six text ", composition_id: comp_one.id, votes: 2, next_ids: [-1], prev_id: 3)
+
+sec_seven = Section.create(text: "Seven text ", composition_id: comp_one.id, votes: 2, next_ids: [-1], prev_id: comp_one.id)
