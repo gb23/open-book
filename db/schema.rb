ade4428e2d06c8ae71c432ea6d50acb53fbe927b
gb23 <gregbenj@hotmail.com>
2018-02-26 15:29:33 -0700

    add about field to db

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180124183452) do
     t.integer "votes"
     t.integer "next_ids", array: true
     t.integer "prev_id"
+    t.boolean "about", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
