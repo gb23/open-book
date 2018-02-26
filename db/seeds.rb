60f1b5557244b19d3d046718dc4f4e427eff9d8d
gb23 <gregbenj@hotmail.com>
2018-02-26 15:29:57 -0700

    add 'about' seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,52 @@
-Section.create(text: "One text ", votes: 1, next_ids: [2,4], prev_id: -1)
+############### About data ###############
+Section.create(text: "ABOUT One text ", votes: 1, next_ids: [2,4], prev_id: -2, about: true)
 
-Section.create(text: "Two text ", votes: 3, next_ids: [3], prev_id: 1)
+Section.create(text: "ABOUT Two text ", votes: 3, next_ids: [3], prev_id: 1, about: true)
 
-Section.create(text: "Three text ", votes: 1, next_ids: [5,6], prev_id: 2)
+Section.create(text: "ABOUT Three text ", votes: 1, next_ids: [5,6], prev_id: 2, about: true)
 
-Section.create(text: "Four text ", votes: 3, next_ids: [-1], prev_id: 1)
+Section.create(text: "ABOUT Four text ", votes: 3, next_ids: [-1], prev_id: 1, about: true)
 
-Section.create(text: "Five text ", votes: 1, next_ids: [-1], prev_id: 3)
+Section.create(text: "ABOUT Five text ", votes: 1, next_ids: [-1], prev_id: 3, about: true)
 
-Section.create(text: "Six text ", votes: 2, next_ids: [-1], prev_id: 3)
+Section.create(text: "ABOUT Six text ", votes: 2, next_ids: [-1], prev_id: 3, about: true)
+
+Section.create(text: "ABOUT IIOne text ", votes: 1, next_ids: [8,10], prev_id: -2, about: true) #7
+
+Section.create(text: "ABOUT IITwo text ", votes: 3, next_ids: [9], prev_id: 7, about: true) #8
+
+Section.create(text: "ABOUT IIThree text ", votes: 1, next_ids: [11,12], prev_id: 8, about: true) #9
+
+Section.create(text: "ABOUT IIFour text ", votes: 3, next_ids: [-1], prev_id: 7, about: true) #10
+ 
+Section.create(text: "ABOUT IIFive text ", votes: 1, next_ids: [-1], prev_id: 9, about: true) #11
+
+Section.create(text: "ABOUT IISix text ", votes: 2, next_ids: [-1], prev_id: 9, about: true) #12
+############### End About data ################
+
+
+Section.create(text: "REAL One text ", votes: 1, next_ids: [14,16], prev_id: -1) #13
+
+Section.create(text: "REAL Two text ", votes: 3, next_ids: [15], prev_id: 13) #14
+
+Section.create(text: "REAL Three text ", votes: 1, next_ids: [17,18], prev_id: 14) #15
+
+Section.create(text: "REAL Four text ", votes: 3, next_ids: [-1], prev_id: 13)#16
+
+Section.create(text: "REAL Five text ", votes: 1, next_ids: [-1], prev_id: 15)#17
+
+Section.create(text: "REAL Six text ", votes: 2, next_ids: [-1], prev_id: 15)#18
 
 
 
-Section.create(text: "seven text ", votes: 1, next_ids: [8,10], prev_id: -1) #7
+Section.create(text: "REAL seven text ", votes: 1, next_ids: [20,22], prev_id: -1) #19
 
-Section.create(text: "eight text ", votes: 3, next_ids: [9], prev_id: 7) #8
+Section.create(text: "REAL eight text ", votes: 3, next_ids: [21], prev_id: 19) #20
 
-Section.create(text: "nine text ", votes: 1, next_ids: [11,12], prev_id: 8) #9
+Section.create(text: "REAL nine text ", votes: 1, next_ids: [23,24], prev_id: 20) #21
 
-Section.create(text: "ten text ", votes: 3, next_ids: [-1], prev_id: 7) #10
+Section.create(text: "REAL ten text ", votes: 3, next_ids: [-1], prev_id: 19) #22
 
-Section.create(text: "eleven text ", votes: 1, next_ids: [-1], prev_id: 9) #11
+Section.create(text: "REAL eleven text ", votes: 1, next_ids: [-1], prev_id: 21) #23
 
-Section.create(text: "twelve text ", votes: 2, next_ids: [-1], prev_id: 9) #12
+Section.create(text: "REAL twelve text ", votes: 2, next_ids: [-1], prev_id: 21) #24
