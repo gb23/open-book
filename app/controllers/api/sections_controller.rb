bb85a01bdc45375b029536ed9691c21ea7735e37
gb23 <gregbenj@hotmail.com>
2018-02-27 11:24:18 -0700

    handle sandboxing 'about' section data manipulation

--- app/controllers/api/sections_controller.rb
+++ app/controllers/api/sections_controller.rb
@@ -2,13 +2,13 @@ class Api::SectionsController < ApplicationController
     before_action :set_section, only:[:show, :update, :destroy]
 
     def index
-        #byebug
+        remove_and_repopulate_about_data
         render json: Section.all.sort
     end
 
     def create
         section = Section.new(section_params)
-        if section.prev_id != -1 
+        if section.prev_id != -1 && section.prev_id != -2
             sectionPrev = Section.find_by(id: section.prev_id)
         else
             sectionPrev = nil
@@ -49,12 +49,53 @@ class Api::SectionsController < ApplicationController
 
 
     private
-
         def set_section 
             @section = Section.find_by(id: params[:id])
         end
 
         def section_params
             params.require(:section).permit(:text, :votes, :prev_id, :about, :next_ids => [] )
+        end
+
+        def remove_and_repopulate_about_data
+            sectionTrues = Section.all.select{ |o| o[:about] == true}
+            Section.delete(sectionTrues) #delete the 'about' data
+            one = Section.create(text: "ABOUT One text ", votes: 1, prev_id: -2, about: true)
+            two = Section.create(text: "ABOUT Two text ", votes: 3, prev_id: one.id, about: true)
+            three = Section.create(text: "ABOUT Three text ", votes: 1, prev_id: two.id, about: true)
+            four = Section.create(text: "ABOUT Four text ", votes: 3, prev_id: one.id, about: true)
+            five = Section.create(text: "ABOUT Five text ", votes: 1, prev_id: three.id, about: true)
+            six = Section.create(text: "ABOUT Six text ", votes: 2, prev_id: three.id, about: true)
+            seven = Section.create(text: "ABOUT IIOne text ", votes: 1, prev_id: -2, about: true) #7
+            eight = Section.create(text: "ABOUT IITwo text ", votes: 3, prev_id: seven.id, about: true) #8
+            nine = Section.create(text: "ABOUT IIThree text ", votes: 1, prev_id: eight.id, about: true) #9
+            ten = Section.create(text: "ABOUT IIFour text ", votes: 3, prev_id: seven.id, about: true) #10
+            eleven = Section.create(text: "ABOUT IIFive text ", votes: 1, prev_id: nine.id, about: true) #11
+            twelve = Section.create(text: "ABOUT IISix text ", votes: 2, prev_id: nine.id, about: true) #12
+            
+            one[:next_ids] = [two.id, four.id]
+            two[:next_ids] = [three.id]
+            three[:next_ids] = [five.id,six.id]
+            four[:next_ids] = [-1]
+            five[:next_ids] = [-1]
+            six[:next_ids] = [-1]
+            seven[:next_ids] = [eight.id,ten.id]
+            eight[:next_ids] = [nine.id]
+            nine[:next_ids] = [eleven.id,twelve.id]
+            ten[:next_ids] = [-1]
+            eleven[:next_ids] = [-1]
+            twelve[:next_ids] = [-1]
+            one.save
+            two.save
+            three.save
+            four.save
+            five.save
+            six.save
+            seven.save
+            eight.save
+            nine.save
+            ten.save
+            eleven.save
+            twelve.save
         end
 end 
